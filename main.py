ed30b954bf8ca794d940229566c8b2185face689
杰洛 <55344116+jieluojun@users.noreply.github.com>
2026-07-27 00:39:46 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 from kivy.uix.webview import WebView
 from kivy.base import runTouchApp
+import time
+import urllib.request
 
-runTouchApp(
-    WebView(url="http://127.0.0.1:5000")
-)
+def is_server_ready():
+    try:
+        urllib.request.urlopen("http://127.0.0.1:5000", timeout=1)
+        return True
+    except:
+        return False
+
+# 等待最多 5 秒
+for i in range(10):
+    if is_server_ready():
+        break
+    time.sleep(0.5)
+
+runTouchApp(WebView(url="http://127.0.0.1:5000"))
